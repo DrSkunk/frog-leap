b72827cada36f45c7a226bbcefca67cc174dbdc1
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2020-05-09 22:35:58 +0200

    done

--- src/App.js
+++ src/App.js
@@ -3,39 +3,111 @@ import styled from 'styled-components';
 import Frog from './Frog';
 
 const frogType = {
-  green: "green",
-  brown: "brown",
-}
+  green: 'green',
+  brown: 'brown',
+};
 
-const Root = styled.div`
+const Wrapper = styled.div`
   position: relative;
   top: 0;
   left: 0;
-`
+`;
 
 const Board = styled.img`
   position: absolute;
   top: 0;
   left: 0;
-`
+`;
 
+const ResetButton = styled.button`
+  position: absolute;
+  top: 418px;
+  left: 0;
+  z-index: 10;
+  font-size: 3em;
+`;
+
+const Congratulations = styled.div`
+  position: absolute;
+  background-color: black;
+  color: white;
+  z-index: 10;
+  top: 140px;
+  left: 104px;
+  padding: 40px;
+  border-radius: 50px;
+  font-size: 3em;
+`;
+
+const initialBoardState = [
+  frogType.green,
+  frogType.green,
+  frogType.green,
+  null,
+  frogType.brown,
+  frogType.brown,
+  frogType.brown,
+];
 
 export default class App extends Component {
-
   state = {
-    board: [
-      frogType.green,
-      frogType.green,
-      frogType.green,
-      null,
-      frogType.brown,
-      frogType.brown,
-      frogType.brown,
-    ]
-  }
+    board: initialBoardState,
+  };
+
+  jump = (frogIndex) => {
+    this.setState((state) => {
+      const frog = state.board[frogIndex];
+      const emptyPlaceIndex = state.board.indexOf(null);
+      const newBoard = JSON.parse(JSON.stringify(state.board));
+      if (frog === frogType.green) {
+        if (frogIndex < emptyPlaceIndex && emptyPlaceIndex - frogIndex <= 2) {
+          newBoard[emptyPlaceIndex] = frogType.green;
+          newBoard[frogIndex] = null;
+          this.setState({ board: newBoard });
+        }
+      } else {
+        if (frogIndex > emptyPlaceIndex && frogIndex - emptyPlaceIndex <= 2) {
+          newBoard[emptyPlaceIndex] = frogType.brown;
+          newBoard[frogIndex] = null;
+          this.setState({ board: newBoard });
+        }
+      }
+    });
+  };
+
+  reset = () => {
+    this.setState({ board: initialBoardState });
+  };
 
   render() {
-    const frogs = this.state.board.map((frog,i) => <Frog key={i} type={frog} index={i}/>)
-    return <Root><Board src="img/background.jpg"/>{frogs}</Root>
+    const { board } = this.state;
+    const frogs = board.map((frog, i) => (
+      <Frog onClick={() => this.jump(i)} key={i} type={frog} index={i} />
+    ));
+    const finishedGame =
+      JSON.stringify(board) ===
+      JSON.stringify([
+        frogType.brown,
+        frogType.brown,
+        frogType.brown,
+        null,
+        frogType.green,
+        frogType.green,
+        frogType.green,
+      ]);
+
+    return (
+      <div>
+        <Wrapper>
+          <ResetButton onClick={this.reset}>Reset</ResetButton>
+
+          <Board src="img/background.jpg" />
+          {frogs}
+        </Wrapper>
+        {finishedGame ? (
+          <Congratulations>Congratulations!</Congratulations>
+        ) : null}
+      </div>
+    );
   }
 }
--- src/Frog.js
+++ src/Frog.js
@@ -4,16 +4,17 @@ import styled from 'styled-components';
 const Img = styled.img`
   position: absolute;
   top: 167px;
-  left: ${props=>props.index * 87}px;
-  border: 1px red solid;
-`
+  left: ${(props) => props.index * 87}px;
+  cursor: pointer;
+  z-index: 10;
+`;
 
 export default class Frog extends Component {
   render() {
-    const {type,index} = this.props
-    if(type === null){
-      return null
+    const { type, index, onClick } = this.props;
+    if (type === null) {
+      return null;
     }
-    return <Img src={`img/frog_${type}.gif`} index={index} />
+    return <Img src={`img/frog_${type}.gif`} index={index} onClick={onClick} />;
   }
 }
