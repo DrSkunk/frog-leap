eb282ccaa3acb11b1e580d29381b3606b9904ff1
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2020-05-09 22:17:43 +0200

    Updates

---
+++ precache-manifest.9f9abf1c7e05d473239b7ff260ba64ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2833387a3d5f252ff6aa8bdd9297be31",
+    "url": "/frog-leap/index.html"
+  },
+  {
+    "revision": "d55d4d2c541dfe41ccd1",
+    "url": "/frog-leap/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dea1e94766394492c2b8",
+    "url": "/frog-leap/static/js/2.3433989f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frog-leap/static/js/2.3433989f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55d4d2c541dfe41ccd1",
+    "url": "/frog-leap/static/js/main.8c353628.chunk.js"
+  },
+  {
+    "revision": "e3b5e66124e31ceaa4ac",
+    "url": "/frog-leap/static/js/runtime-main.8d613f67.js"
+  }
+]);
